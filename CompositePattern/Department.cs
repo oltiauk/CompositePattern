a18c1bf6476414f1aeea5a1b6f35ff9d24224b11
oltigjaka <102356108+oltiauk@users.noreply.github.com>
2024-12-05 18:03:42 +0100

    Department and Employee

---
+++ CompositePattern/Department.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Department : IEmployeeSortable
+{
+    private List<Employee> employees = new List<Employee>();
+    private string _name;
+
+    public Department(string name)
+    {
+        _name = name;
+    }
+
+    public void Add(Employee employee)
+    {
+        employees.Add(employee);
+    }
+
+    public void Remove(Employee employee)
+    {
+        employees.Remove(employee);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Department: {_name}");
+        foreach (var employee in employees)
+        {
+            employee.Display();
+        }
+    }
+
+    // Implementing the IEmployeeSortable interface
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public decimal GetWage()
+    {
+        // Departments may not have wages, so you can return a default value or throw an exception
+        return 0.0m; // Default value, adjust as needed
+    }
+}
---
+++ CompositePattern/EmployeeSorting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EmployeeSortingService
+{
+    public void SortByName(List<Employee> employees)
+    {
+        employees.Sort((e1, e2) => e1.GetName().CompareTo(e2.GetName()));
+    }
+
+    public void SortByWage(List<Employee> employees)
+    {
+        employees.Sort((e1, e2) => e1.GetWage().CompareTo(e2.GetWage()));
+    }
+}
