970de18dc75a52138edac62ec8d6f99ec02b3719
oltigjaka <102356108+oltiauk@users.noreply.github.com>
2024-12-05 18:06:02 +0100

    The one step behind commit

---
+++ CompositePattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var worker1 = new Worker("John", new Wage(50000.00m));
+        var worker2 = new Worker("Alice", new Wage(60000.00m));
+        var manager1 = new Manager("Bob", new Wage(80000.00m));
+        var manager2 = new Manager("Charlie", new Wage(70000.00m));
+
+        var employees = new List<Employee> { worker1, worker2, manager1, manager2 };
+
+        // Display unsorted employees
+        Console.WriteLine("Unsorted Employees:");
+        foreach (var employee in employees)
+        {
+            employee.Display();
+        }
+
+        // Sort employees by name
+        var sortingService = new EmployeeSortingService();
+        sortingService.SortByName(employees);
+
+        // Display employees after sorting by name
+        Console.WriteLine("\nEmployees Sorted by Name:");
+        foreach (var employee in employees)
+        {
+            employee.Display();
+        }
+
+        // Sort employees by wage
+        sortingService.SortByWage(employees);
+
+        // Display employees after sorting by wage
+        Console.WriteLine("\nEmployees Sorted by Wage:");
+        foreach (var employee in employees)
+        {
+            employee.Display();
+        }
+    }
+}
---
+++ CompositePattern/Worker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Worker : Employee
+{
+    public string _name;
+    public Wage WorkerWage { get; } // New property
+
+    // Private constructor to enforce the use of the factory method
+    public Worker(string name, Wage wage)
+    {
+        _name = name;
+        WorkerWage = wage;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Worker: {_name}, Wage: {WorkerWage}");
+    }
+
+    // Factory method to create Worker instances
+    public static Worker CreateWorker(string name, decimal wageAmount)
+    {
+        return new Worker(name, new Wage(wageAmount));
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public decimal GetWage()
+    {
+        return WorkerWage.Amount;
+    }
+}
