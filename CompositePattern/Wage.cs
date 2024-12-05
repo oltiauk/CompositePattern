99a4a73927555c4caa8ca093b284a7b209dd4a60
oltigjaka <102356108+oltiauk@users.noreply.github.com>
2024-12-05 18:06:35 +0100

    The PR/MR request

---
+++ CompositePattern/Wage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Wage
+{
+    public decimal Amount { get; }
+
+    public Wage(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Wage amount cannot be negative.");
+        }
+
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"{Amount:C}";
+    }
+}
+
