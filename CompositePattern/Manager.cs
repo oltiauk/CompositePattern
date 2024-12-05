6393be6153cfa286440b111b57e3daabd8eb77b9
oltigjaka <102356108+oltiauk@users.noreply.github.com>
2024-12-05 18:04:19 +0100

    New Develop Branch

---
+++ CompositePattern/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Manager : Employee, IEmployeeSortable
+{
+    private string _name;
+    public Wage ManagerWage { get; private set; }
+
+    // Constructor that takes name and wage parameters
+    public Manager(string name, Wage wage)
+    {
+        _name = name;
+        ManagerWage = wage;
+    }
+
+    // Implementing the IEmployeeSortable interface
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public decimal GetWage()
+    {
+        return ManagerWage.Amount;
+    }
+
+    // Implementing the Employee interface
+    public void Display()
+    {
+        Console.WriteLine($"Manager: {_name}, Wage: {ManagerWage}");
+    }
+
+    public string GetDetails()
+    {
+        return $"Name: {_name}, Role: Manager";
+    }
+}
